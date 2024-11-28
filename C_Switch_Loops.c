94a2070ac4695076f2cea9ce0d62128775ce7175
Ashutosh Pandey <asusolan2016@gmail.com>
2024-11-28 09:46:42 +0530

    2nd assignment

---
+++ C_Switch_Loops.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(){
+    // Day of week using switch case
+    /*int a;
+    printf("Enter week day: ");
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+    }*/
+
+    //Days in a month 
+    /*int a;
+    printf("Enter Month Number: ");
+    scanf("%d",&a);
+    switch (a){
+        case 1||3||5||7||8||10||12:
+            printf("31 Days");
+            break;
+        case 2:
+            printf("28 or 29 days depending on month");
+            break;
+        default:
+            printf("30 days");
+    }*/
+}
