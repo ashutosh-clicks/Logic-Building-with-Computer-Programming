61705b7e9de1ed606c8663f16cfe77431b2a91dc
Ashutosh Pandey <asusolan2016@gmail.com>
2024-12-10 20:14:14 +0530

    Loop 14

--- C_Loops.c
+++ C_Loops.c
@@ -82,4 +82,66 @@ int main(){
     }*/
 
    
+    //Loop to count digits in a number
+    /*int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    int count = 0;
+    while(a!=0){
+        a = a/10;
+        count++;
+    }
+    printf("%d",count);*/
+
+    //Loop to print first and last digit of a number
+    /*int a,f,l,temp;
+    f = 0;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    temp = a;
+    while(a>=10){
+        f = a/10;
+        a  = f;
+        // printf("%d",f);
+    }
+    
+    l = temp%10;
+    
+    printf("First: %d\nLast: %d", f,l);*/
+
+    
+    //Loop to find the sum of first and last digits of a number
+    /*int a,f,l,temp;
+    f = 0;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    temp = a;
+    while(a>=10){
+        f = a/10;
+        a  = f;
+        // printf("%d",f);
+    }
+    
+    l = temp%10;
+    int sum = l+f;
+    printf("The sum is: %d",sum);*/
+
+
+    //Loop to swap first and last digits of a number
+    //Naaaah🗣️🗣️🗣️🗣️
+
+
+    //Loop to find sum of all digits of a number
+    /*int n,sum,temp;
+    printf("Enter a number");
+    scanf("%d",&n);
+    temp = n;
+    sum = 0;
+    while(n!=0){
+        sum += n % 10;
+        n = n / 10;
+    }
+    printf("%d",sum);*/
+
+    
 }
