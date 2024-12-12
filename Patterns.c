fb0f5d65df9b01e14a616c589446149eb4d0b93b
Ashutosh Pandey <asusolan2016@gmail.com>
2024-12-12 20:26:27 +0530

    Pattern FInal

--- Patterns.c
+++ Patterns.c
@@ -113,10 +113,110 @@ int main(){
 
 
     //No.8
-    for(int i = 1;i<=5;i++){
+    /*for(int i = 1;i<=5;i++){
         for(int j = 1; j<=5; j++){
-
+            if(i == j){
+                printf("1");
+            }
+            else{
+                if(i == 1 && j ==5 || i ==5 && j== 1 ||
+                i == 2 && j ==4 || i ==4 && j ==2){
+                    printf("1");
+                }
+                else{
+                    printf("0");
+                }
+            }
+        
         }
-    }
+            printf("\n");
+    }*/
 
+
+    //No.9
+    /*for(int i =1; i<=5;i++){
+        for(int j = 1;j<=5;j++){
+            if((i == 5 || i == 1) && (j ==1 || j==5)){
+                printf("0");
+            }
+            else if((i>1 && i <5) && (j>1 && j<5)){
+                printf("0");
+            }
+            else{
+                printf("1");
+            }
+        }
+        printf("\n");
+    }*/
+
+    //No.10
+    /*for(int i =1; i<=5;i++){
+        for(int j = 1;j<=5;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+    }*/
+
+    //No.11
+    /*for(int i =1; i<=5;i++){
+        for(int j = 1;j<=5;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }*/
+
+
+    //No.12
+    /*int t = 0;
+    for(int i =1; i<=5;i++){
+        for(int j = 1;j<=5;j++){
+            printf("%d",j+t);
+        }
+        t = t+1;
+        printf("\n");
+    }*/
+
+    
+    //No.13
+    /*int t = 1;
+    for(int i =1; i<=5;i++){
+        for(int j = 1;j<=5;j++){
+            printf("%d",t);
+            t = t+1;
+        }
+        
+        printf("\n");
+    }*/
+
+    
+    //No.14
+    /*for(int i=1; i<=5; i++)
+    {
+        for(int j=5; j>5-i; j--){
+            printf("%d", j);
+        }
+
+        for(int j=1; j<=5-i; j++){
+            printf("%d", (5 - i + 1));
+        }
+
+        printf("\n");
+    }*/
+
+
+    //No.15
+    /*for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            printf("%d", j);
+        }
+
+        for(int j=i; j>1; j--)
+        {
+            printf("%d", 5);
+        }
+
+        printf("\n");
+    }*/
 }
