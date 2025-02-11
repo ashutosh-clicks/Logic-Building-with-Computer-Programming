41432350932a151e075cb7a45a41417a2c2f7560
Ashutosh Pandey <asusolan2016@gmail.com>
2025-02-11 19:40:00 +0530

    Hackerrank

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-use namespace std;
+#include <string>
+using namespace std;
 
-int main(){
+int main() {
+	// Complete the program
+    string a,b;
+    a = "abcd";
+    b = "ef";
+    string c = a+b;
+    cout<<a.length()<<" "<<b.length()<<endl;
+    cout<<c<<endl;
+    for(int i = 0; i<a.length();i++){
+        if(i == 0 ){
+            cout<<b[0];
+        }
+        else{
+            cout<<a[i];
+        }
+    }
+    cout<<" ";
 
-
+    for(int j = 0; j<b.length();j++){
+        if(j == 0 ){
+            cout<<a[0];
+        }
+        else{
+            cout<<b[j];
+        }
+    }
+  
     return 0;
 }
