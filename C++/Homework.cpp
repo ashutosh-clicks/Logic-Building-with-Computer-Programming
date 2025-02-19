48bedb8fca1f80becbf3230b69117152cfe3ca58
Ashutosh Pandey <asusolan2016@gmail.com>
2025-02-19 11:19:41 +0530

    Update Homework.cpp

--- C++/Homework.cpp
+++ C++/Homework.cpp
@@ -3,30 +3,39 @@
 using namespace std;
 
 int main() {
-	string binary;
-    cin>>binary;
+	// string binary;
+    // cin>>binary;
 
 
-    int count = binary.length();
-    /*for(int i = 0; binary > 0; i++){
-        count++;
-        binary = binary/10;
-    }*/
-    int power = pow(2,count-1);
-    int j = -1;
-    for(int i = (count-1); i>=0; i--){
-        // cout<<binary[i]<<endl;
-        j++;
-        if(binary[i] == 49 and i!= 0){
-            power = power + (pow(2,j));
+    // int count = binary.length();
+    // /*for(int i = 0; binary > 0; i++){
+    //     count++;
+    //     binary = binary/10;
+    // }*/
+    // int power = pow(2,count-1);
+    // int j = -1;
+    // for(int i = (count-1); i>=0; i--){
+    //     // cout<<binary[i]<<endl;
+    //     j++;
+    //     if(binary[i] == 49 and i!= 0){
+    //         power = power + (pow(2,j));
 
-            // power++;
-        }
+    //         // power++;
+    //     }
 
+    // }
+    // // cout<<power<<endl;
+
+    // printf("%d",power);
+
+
+    int number;
+    int count;
+    for (int i = 0; number > 0; i++) {
+            count++;
+            number = number/10;
     }
-    // cout<<power<<endl;
-
-    printf("%d",power);
-
+    int i = 
+    int power  = pow(number[1],power);
     return 0;
 }
