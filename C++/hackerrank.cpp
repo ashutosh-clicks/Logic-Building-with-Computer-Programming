77bab7b222487377f22a68f89c041906f4127d36
Ashutosh Pandey <asusolan2016@gmail.com>
2025-02-27 17:07:56 +0530

    hackerrank

--- C++/hackerrank.cpp
+++ C++/hackerrank.cpp
@@ -1,49 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class Box{
-   private:
-   int l,b,h;
-   
+class Rectangle{
    public:
-   void box(){
-      l = 0;
-      b = 0;
-      h = 0;
+       int width;
+       int height;
+   void display(){
+      width = Rectangle::width;
+      height = Rectangle::height;
+      cout<<width<<" "<<height<<endl;
    }
-   void boxlbh(int x, int y, int z){
-      l = x;
-      b = y;
-      h = z;
-   }
-   int getLength(){
-      cout<<l<<endl;
-      return 1;
-   }
-   int getBreadth(){
-      cout<<b<<endl;
-      return 1;
-   }
-   int getHeight(){
-      cout<<h<<endl;
-      return 1;
-   }
-   long long CalculateVolume(){
-      cout<<(l*b*h)<<endl;
-      return 1;
-   }
-  };
-
-int main() {
+ 
+};
+class RectangleArea : public Rectangle{
+    public:
     
-  Box b1;
-  Box b2;
-  b1.boxlbh(2,3,4);
-  b2.box();
-  b2.getLength();
-  b2.getBreadth();
-  b2.getHeight();
-  b2.CalculateVolume();
-  
-  return 0;
+    void read_input(){
+      cin>>width;
+      cin>>height;
+    }
+    void display(){
+      cout<<width*height;
+    }
+};  
+
+int main(){
+   RectangleArea r_area;
+   r_area.read_input();
+
+   // r_area.Rectangle::display();
+   r_area.display();
+
+   return 0;
 }
