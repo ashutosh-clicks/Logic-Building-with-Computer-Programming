2884383b3087addbde6eeb345341a18be9f0e9c7
Ashutosh Pandey <asusolan2016@gmail.com>
2025-02-28 19:10:18 +0530

    Hackerank vector questions

--- DSA/DSA_OOPS.cpp
+++ DSA/DSA_OOPS.cpp
@@ -8,17 +8,27 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n;   
+    int n;
     cin>>n;
-    int arr[n];
-    for(int i = 0; i<=n;i++){
-      arr[i] = i;
+    
+    vector <int> v(n);   
+    for(int i =0;i<n;i++){
+        cin>>v[i];
     }
     
-    for(int i = 0; i<=n;i++){
-      int arr2[p];
-      
+    int q;
+    cin>>q;
+    int y[q];
+    for(int i = 0;i < q;i++){
+        cin>>y[i];
     }
-
+    
+    for(int j =0; j<q;j++){
+        for(int k = 0; k<n;k++){
+            if(y[j] == v[k]){
+                cout<<"Yes"<<" "<<k;
+            }
+        }
+    }
     return 0;
 }
