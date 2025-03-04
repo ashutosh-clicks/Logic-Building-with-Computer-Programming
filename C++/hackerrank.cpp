d7520565f439f4a03c3f820729d9189528b2030f
Ashutosh Pandey <asusolan2016@gmail.com>
2025-03-04 17:58:38 +0530

    NUDES

--- C++/hackerrank.cpp
+++ C++/hackerrank.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-   public:
-   int data;
-   Node* next;
+class Node {
+public:
+    int data;
+    Node* next;
 
-   Node(int val){
-      data = val;
-      next = NULL;
-   }
-
+    Node(int val) {  // Constructor to initialize node
+        data = val;
+        next = NULL;
+    }
 };
 
-void linkedList(Node* head){
-   Node* temp =  head;
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
 
-   if(head != NULL){
-      cout<<temp->data<<"->";
-      temp = temp->next;
-   }
-   cout<<"NULL"<<endl;
+int main() {
+    // Creating nodes
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
 
-};
+    // Linking nodes
+    head->next = second;
+    second->next = third;
 
-int main(){
-   Node* head = new Node(10);
-   Node *second = new Node(20);
-   Node* third = new Node(30);
+    // Print list
+    printList(head);
 
-   head->next = second;
-   second->next = third;
-   
-   linkedList(head);
-   
+    return 0;
 }
--- DSA/DSA_OOPS.cpp
+++ DSA/DSA_OOPS.cpp
@@ -1,45 +1,41 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-class Insertion{
-    private:
-    int size = 5;
-    int arr[5] = {1,2,3,4};
-    int x;
+class Node{
+   public:
+   int data;
+   Node* next;
+   Node* prev;
 
-    public:
-    void insertAtBeg(){
-        cout<<"Enter desired value: ";
-        for(int i = (size-1);i>=0;i--){
-            arr[i+1] = arr[i];
-        }
-        cin>>x;
-        arr[0] = x;
-        for(auto i: arr){
-            cout<<arr[i]<<" ";
-        }
-    }
+   Node(int val){
+      data = val;
+      next = NULL;
+      prev = NULL;
+   }
 
-    void insertAtEnd(){
-        cout<<"Enter desired value: ";
-        cin>>x;
-        arr[size-1] = x;
-        for(int i = 0;i<=size-1; i++){
-            cout<<arr[i]<<" ";
-        }
-
-    }
 };
-    
-int main() {
-    Insertion i1;
-    Insertion i2;
-    // i1.insertAtBeg();
-    i2.insertAtEnd();
 
-    return 0;
+void linkedList(Node* head){
+   Node* temp =  head;
+
+   while(head != NULL){
+      cout<<temp->data<<"->";
+      temp = temp->prev;
+   }
+   cout<<"NULL"<<endl;
+
+};
+
+int main(){
+   Node* head = new Node(10);
+   Node *second = new Node(20);
+   Node* third = new Node(30);
+
+   head->next = second;
+   second->next = third;
+   third->prev = second;
+   second->prev = head;
+   
+   linkedList(third);
+   
 }
