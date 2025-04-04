e90e1c97bfa3e99015e517151a98467a2acaab19
Ashutosh Pandey <asusolan2016@gmail.com>
2025-04-04 15:02:56 +0530

    push

--- DSA/LinkedList.cpp
+++ DSA/LinkedList.cpp
@@ -11,10 +11,12 @@ class Node{
     Node(){
         data = 0;
         next = NULL;
+        prev = NULL;
     }
     Node(int a){
         data  = a;
         next = NULL;
+        prev = NULL;
     }
 
 };
@@ -29,63 +31,84 @@ void printForward(Node* head){
     cout<<"NULL";
 }
 
-
-Node* insertAtBegin(Node *head, int newData){
-
-    Node* newNode = new Node(newData); // Create new node
-    newNode->next = head;             // Point the new node to the current head
+Node* insertAtBegin(Node*head,int data){
+    Node* newNode = new Node(data);
+    newNode->next = head;
     return newNode;
-
-
 }
 
-Node *insertAtEnd(Node * head,int newData){
-    Node* newNode = new Node(newData);
-    if(head == NULL){
-        return newNode;
-    }
+
+Node* insertAtEnd(Node* head, int data){
+    Node* newNode = new Node(data);
     Node* temp = head;
-    while (temp->next != nullptr) { // Traverse to the last node
+    while(temp->next){
         temp = temp->next;
     }
-    temp->next = newNode; // Update the last node's next pointer
-    return head;          // Return the original head
-
-}
-
-Node* insertAtValue(Node* head, int newData, int value){
-    Node *temp = head;
-    while(temp!= NULL){
-        if(temp->data!=value){
-            temp = temp->next;
-        }
-        else{
-            temp->data = newData;
-        }
-
-    }
+    temp->next = newNode;
     return head;
-
 }
 
-Node* insertBeforeValue(Node *head, int newData, int value){
+Node* insertAtPosition(Node* head, int pos, int data){
     Node* temp = head;
-    Node* tempest = temp->next;
-    // Node* newNode = new Node(newData);
-    while(temp!=NULL){
-        if(tempest->data == value){
-            temp->data = newData;
-            temp = temp->next;
-        }
-        else{
-            temp = temp->next;
-        }
-
+    for(int i = 1; i<pos-1;i++){
+        temp = temp->next;
     }
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
     return head;
+}
+
+Node* reverse(Node* head){
+    Node* temp = head;
+    Node* next = NULL;
+    Node* prev = NULL;
+    while(temp!=NULL){
+        next = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next;
+    }
+    return prev;
+}
+
+Node* insertBeforePosition(Node* head,int pos,int value){
+    Node* temp  = head;
+    if(pos==0){
+        insertAtBegin(head,value);
+        return head;
+    }
+    else{
+        for(int i = 1;i<pos-2;i++){
+            temp = temp->next;
+        }
+        Node* newNode = new Node(value);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        return head;
+    }
 
 }
 
+void printBackward(Node* tail){
+    Node* temp = tail;
+    while(temp != NULL){
+        cout<<temp->data<<"<-";
+        temp = temp->prev;
+    }
+    cout<<"NULL";
+
+}
+
+void centerElement(Node* head){
+    Node* f,*s;
+    f = s = head;
+    while(f and f->next){
+        s = s->next;
+        f = f->next->next;
+    }
+    cout<<s->data<<endl;
+}
 
 int main(){
     //Linked lIst
@@ -99,18 +122,25 @@ int main(){
     n2->next = n3;
     n3->next = n4;
     n4->next = n5;
+
+    n5->prev = n4;
+    n4->prev = n3;
+    n3->prev = n2;
+    n2->prev = n1;
     
+    Node* tail = n5;
     Node *head = n1;
 
     //Linkd List
     head = insertAtBegin(head,100);
-    // head = insertAtEnd(head,200);
-    // head = insertAtValue(head,300,50);
-    // head = insertAtValue(head, 400, 40);
-    head = insertBeforeValue(head,500,30);
+    head = insertAtEnd(head,200);
+    head = insertAtPosition(head,2,300);
+    head = insertBeforePosition(head,2,400);
+    // head = reverse(head);
+    centerElement(head);
     printForward(head);
+    // printBackward(tail);
 
-
-
+    
     return 0;
 }
